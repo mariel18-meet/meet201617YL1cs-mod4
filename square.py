5be2b2e52190d8d28ae8abb20ff94c7998c0a027
mariel18-meet <marial-basous@hotmail.com>
2017-01-18 18:56:29 +0200

    hi

--- square.py
+++ square.py
@@ -1,4 +1,14 @@
-from​ rectangle import​ Rectangle
+from rectangle import Rectangle
+
 class Square(Rectangle):
-      def __init__(self,lenght)           
+      def __init__(self,length):
+            super(Square,self).__init__(length,length)
+
+      def set_length(self,new_length):
+            super(Square,self).set_length(new_length)
+            super(Square,self).set_height(new_length)
+      def set_height(self,new_height):
+            super(Square,self).set_length(new_height)
+            super(Square,self).set_height(new_height)
+            
     
